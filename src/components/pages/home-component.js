108fd20cb55043ec5d7db00d810bd1905abda131
Rishi Ajoykumar <rishiajoykumar@Rishis-MacBook-Pro.local>
2024-10-03 11:25:40 -0500

    Movie page is functional. Also made some changes to handleKeyDown in HomeComponent to help process userInput and ensure consistency accross components

--- src/components/pages/home-component.js
+++ src/components/pages/home-component.js
@@ -1,17 +1,19 @@
 import { fetchMoviesBySearch } from "../../services/OMDB-SERVICE.js";
 import React, { useState } from "react";
 
-function Home () {
+function Home() {
   const [movieList, setMovieList] = useState([]);
 
-  async function handleKeyDown (e) {
+  async function handleKeyDown(e) {
     if (e.key === "Enter") {
+      const userInput = e.target.value.toString().trim().toLowerCase();
       try {
-        const result = await fetchMoviesBySearch(e.target.value.toString());
+        const result = await fetchMoviesBySearch(userInput);
         setMovieList(result.Search);
-      }
-      catch {
-        console.log("Error: could not fetch movies by search!");
+      } catch {
+        console.log(
+          `Error: could not fetch movies by searchQuery ${userInput}!`
+        );
       }
       e.target.value = "";
     }
@@ -22,17 +24,22 @@ function Home () {
       <h1>Home</h1>
       <input type="text" onKeyDown={handleKeyDown} />
       <ul>
-        {movieList.map(movie => (
+        {movieList.map((movie) => (
           <li key={movie.imdbID}>
             <img src={movie.Poster} alt={movie.Title} />
             <p>{movie.Title}</p>
             <p>{movie.Year}</p>
-            <a href={`https://www.imdb.com/title/${movie.imdbID}`} target="_blank">IMDB</a>
+            <a
+              href={`https://www.imdb.com/title/${movie.imdbID}`}
+              target="_blank"
+            >
+              IMDB
+            </a>
           </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default Home;
--- src/components/pages/movie-component.js
+++ src/components/pages/movie-component.js
@@ -1,5 +1,43 @@
-function Movie () {
-  return <h1>Movie</h1>
+import { fetchSpecificMovie } from "../../services/OMDB-SERVICE.js";
+import React, { useState } from "react";
+
+function Movie() {
+  const [movie, setMovie] = useState(null);
+
+  async function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      const userInput = e.target.value.trim().toLowerCase();
+      try {
+        const result = await fetchSpecificMovie(userInput);
+        setMovie(result);
+      } catch (error) {
+        console.log(`Error: unable to retrieve results for ${userInput}`);
+      }
+      e.target.value = "";
+    }
+  }
+
+  return (
+    <div>
+      <h1>Movie</h1>
+      <input type="text" onKeyDown={handleKeyDown} />
+      {movie && (
+        <div>
+          <img src={movie.Poster} alt={movie.Title} />
+          <p>{movie.Title}</p>
+          <p>Release Year: {movie.Year}</p>
+          <a
+            href={`https://www.imdb.com/title/${movie.imdbID}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            IMDB
+          </a>
+          <p>{movie.Plot}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Movie;

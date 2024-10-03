164faf98df8a1c79840591e40268266f4ab05446
Rishi Ajoykumar <rishiajoykumar@Rishis-MacBook-Pro.local>
2024-10-03 11:29:41 -0500

    NotFoundComponent is now complete. Minimum Viable Product achieved.

--- src/components/pages/not-found-component.js
+++ src/components/pages/not-found-component.js
@@ -1,5 +1,11 @@
-function NotFound () {
-  return <h1>Not Found</h1>
+function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
 }
 
 export default NotFound;

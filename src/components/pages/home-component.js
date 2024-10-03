53f19da6fbdaf9233ba56f98076fb22e866522ec
Rishi Ajoykumar <rishiajoykumar@Rishis-MacBook-Pro.local>
2024-10-03 11:06:40 -0500

    Skeletal movie page is working

--- src/components/pages/home-component.js
+++ src/components/pages/home-component.js
@@ -1,5 +1,38 @@
+import { fetchMoviesBySearch } from "../../services/OMDB-SERVICE.js";
+import React, { useState } from "react";
+
 function Home () {
-  return <h1>Home</h1>
+  const [movieList, setMovieList] = useState([]);
+
+  async function handleKeyDown (e) {
+    if (e.key === "Enter") {
+      try {
+        const result = await fetchMoviesBySearch(e.target.value.toString());
+        setMovieList(result.Search);
+      }
+      catch {
+        console.log("Error: could not fetch movies by search!");
+      }
+      e.target.value = "";
+    }
+  }
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <input type="text" onKeyDown={handleKeyDown} />
+      <ul>
+        {movieList.map(movie => (
+          <li key={movie.imdbID}>
+            <img src={movie.Poster} alt={movie.Title} />
+            <p>{movie.Title}</p>
+            <p>{movie.Year}</p>
+            <a href={`https://www.imdb.com/title/${movie.imdbID}`} target="_blank">IMDB</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Home;
